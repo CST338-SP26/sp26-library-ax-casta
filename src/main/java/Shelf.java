b1919970adf907b0cd1ff6c6cefcd7a3848d9bf5
ax-casta <xandercasta07@yahoo.com>
2026-04-05 16:39:33 -0700

    added constructors and comments

--- src/main/java/Shelf.java
+++ src/main/java/Shelf.java
@@ -4,15 +4,29 @@ import java.util.HashMap;
 import java.util.Objects;
 
 public class Shelf {
+    //CONSTANTS
     public static final int SHELF_NUMBER_ = 0;
     public static final int SUBJECT_ = 1;
+
+    //VARIABLES
     private HashMap<Book, Integer> books;
     private int shelfNumber;
     private String subject;
 
+    //CONSTRUCTORS
+    public Shelf(int shelfNumber, String subject) {
+        this.subject = subject;
+        this.shelfNumber = shelfNumber;
+        this.books = new HashMap<>();
+    }
+    public Shelf() {
+    }
+
+    //METHODS
     public Code addBook(Book book){return null;}
     public int getBookCount(Book book){return 0;}
 
+    //EQUALS
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -20,16 +34,19 @@ public class Shelf {
         return shelfNumber == shelf.shelfNumber && Objects.equals(subject, shelf.subject);
     }
 
+    //HASHCODE
     @Override
     public int hashCode() {
         return Objects.hash(shelfNumber, subject);
     }
 
+    //TO-STRING
     @Override
     public String toString(){
         return shelfNumber + " : " + subject;
     }
 
+    //GETTERS AND SETTERS
     public HashMap<Book, Integer> getBooks() {
         return books;
     }
