489e322aae101831f351968d1fffba0d05c2f1dc
ax-casta <xandercasta07@yahoo.com>
2026-04-05 20:00:38 -0700

    added test file, fixed formatting, added new comments fixed testing issues, passed tests

--- src/main/java/Shelf.java
+++ src/main/java/Shelf.java
@@ -3,6 +3,12 @@ import Utilities.Code;
 import java.util.HashMap;
 import java.util.Objects;
 
+/**
+ * Author: Alexander Castaneda
+ * Title: Project_01 Part 3 - Shelf
+ * Date: 4/5/2026
+ */
+
 public class Shelf {
     //CONSTANTS
     public static final int SHELF_NUMBER_ = 0;
@@ -14,15 +20,32 @@ public class Shelf {
     private String subject;
 
     //CONSTRUCTORS
+    /**
+     * Creates a Shelf with a shelf number and subject
+     *
+     * @param shelfNumber the shelf number
+     * @param subject the book subject
+     */
     public Shelf(int shelfNumber, String subject) {
         this.subject = subject;
         this.shelfNumber = shelfNumber;
         this.books = new HashMap<>();
     }
+    /**
+     * Creates an empty shelf
+     */
     public Shelf() {
+        this.books = new HashMap<>();
     }
 
-    //ADDBOOK
+    //ADD-BOOK
+    /**
+     * Adds book to the shelf if the subject matches the shelf.
+     * If book already exists, increments count.
+     *
+     * @param book the book being added to the shelf
+     * @return Code.SUCCESS if the book is added, or Code.SHELF_SUBJECT_MISMATCH_ERROR if subject does not match
+     */
     public Code addBook(Book book){
         //checks if hashmap contains book
         if (books.containsKey(book)){
@@ -43,7 +66,13 @@ public class Shelf {
         }
     }
 
-    //REMOVEBOOK
+    //REMOVE-BOOK
+    /**
+     * removes a book from the shelf if the book is on the shelf
+     *
+     * @param book the book being removed
+     * @return Code.BOOK_NOT_IN_INVENTORY_ERROR if book is not on the shelf or if there are no copies of the book, or Code.SUCCESS if book is removed.
+     */
     public Code removeBook(Book book){
         //checks if hashmap does not contain book
         if (!books.containsKey(book)){
@@ -63,29 +92,35 @@ public class Shelf {
         }
     }
 
-    //GETBOOKCOUNT
+    //GET-BOOK-COUNT
+    /**
+     * returns number of copies of a book on shelf.
+     *
+     * @param book the book to check
+     * @return the number of copies, or -1 if the book does not exist
+     */
     public int getBookCount(Book book){
         //checks if hashmap does not contain book
-        if (!books.containsKey(book)) {
-            return -1;
-        }
         //returns book
-        else{
-            return books.get(book);
-        }
+        return books.getOrDefault(book, -1);
     }
 
-    //LISTBOOKS
+    //LIST-BOOKS
+    /**
+     * Returns a formatted list of the books on shelf.
+     *
+     * @return a string listing all books and their counts on the shelf
+     */
     public String listBooks(){
         int bookNum = 0;
-        String bookList = "";
+        StringBuilder bookList = new StringBuilder();
         //builds bookNum
         for(Integer count : books.values()){
             bookNum += count;
         }
         //builds bookList
         for(Book book : books.keySet()){
-            bookList = bookList + "\n" + book.toString() + " " + books.get(book);
+            bookList.append("\n").append(book.toString()).append(" ").append(books.get(book));
         }
         //case if bookNum is 1
         if (bookNum == 1){
@@ -98,6 +133,12 @@ public class Shelf {
     }
 
     //EQUALS
+    /**
+     * Compares this shelf to another object.
+     *
+     * @param o the object being compared
+     * @return true if the shelves have the same shelf number and subject
+     */
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -106,12 +147,22 @@ public class Shelf {
     }
 
     //HASHCODE
+    /**
+     * Returns a hash code for this shelf.
+     *
+     * @return a hash code based on shelf number and subject
+     */
     @Override
     public int hashCode() {
         return Objects.hash(shelfNumber, subject);
     }
 
     //TO-STRING
+    /**
+     * Returns a string for the shelf.
+     *
+     * @return the shelf number and subject as [NUMBER] : [SUBJECT]
+     */
     @Override
     public String toString(){
         return shelfNumber + " : " + subject;
